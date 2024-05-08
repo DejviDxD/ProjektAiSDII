3cccaebe38d3b3e720eb08b02fe26d8befd46304
Michal <mkrkoko2@gmail.com>
2024-05-08 09:55:26 +0200

    BoyerMoore'

---
+++ algorytmy/Boyer-Moore/Boyer_Moore.py
@@ -0,0 +1,57 @@
+class BoyerMoore:
+
+    def __init__(self,t,p):
+        self.pattern = p
+        self.text = t
+    def preprocess_strong_suffix(self,shift, bpos, m):
+        i = m
+        j = m + 1
+        bpos[i] = j
+        while i > 0:
+            while j <= m and self.pattern[i - 1] != self.pattern[j - 1]:
+
+                if shift[j] == 0:
+                    shift[j] = j - i
+                j = bpos[j]
+            i -= 1
+            j -= 1
+            bpos[i] = j
+
+    def preprocess_case2(self,shift, bpos, m):
+        j = bpos[0]
+        for i in range(m + 1):
+            if shift[i] == 0:
+                shift[i] = j
+
+            if i == j:
+                j = bpos[j]
+
+
+    def BM(self):
+        s = 0
+        m = len(self.pattern)
+        n = len(self.text)
+
+        bpos = [0] * (m + 1)
+        shift = [0] * (m + 1)
+        self.preprocess_strong_suffix(shift, bpos, m)
+        self.preprocess_case2(shift, bpos, m)
+        pp = 0
+        while s <= n - m:
+            j = m - 1
+            while j >= 0 and self.pattern[j] == self.text[s + j]:
+                j -= 1
+            if j < 0:
+                print("Wzorzec wystepuje z przesunieciem = %d" % s)
+                pp += s
+                s += shift[0]
+            else:
+                s += shift[j + 1]
+        if(pp == 0):
+            print("Wzorzec nie wystepuje w tekscie")
+
+if __name__ == "__main__":
+    text = "ABAAAABAACD"
+    pat = "ABA"
+    search = BoyerMoore(text,pat)
+    search.BM()
